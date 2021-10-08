c9d80477c301790a4cefdc65664b0e0a9bf7a465
Flopinator <flo.fressner@gmail.com>
2021-10-08 19:59:17 +0200

    Class Calculator erstellt

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 20;
+        boolean c = true;
+        char d = 'c';
+        String e = "Hallo";
+
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
