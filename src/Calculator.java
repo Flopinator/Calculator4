ec8a0c5a5781ebf819c1aab44ee0e07ca0f7470a
Flopinator <flo.fressner@gmail.com>
2021-10-09 10:02:17 +0200

    Update dividieren

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,6 +15,8 @@ public class Calculator {
         System.out.println(a);
         Arithmetic(10,20);*/
         printResult(Arithmetic(10,20));
+        //dividieren(10,2);
+        printDividieren(dividieren(10,2));
     }
 
     public static int Arithmetic (int a, int b){
@@ -32,6 +34,29 @@ public class Calculator {
     }
 
     public static void printResult(int result){
+
         System.out.println(result);
     }
+
+    public static int dividieren(int divident, int divisor) {
+        if (divisor != 0) {
+            return (divident / divisor);
+        }
+        else{
+            return 0;
+        }
+
+    }
+
+    public static void printDividieren(int erg)
+    {
+        if (erg != 0)
+        {System.out.println("Ergebnis ist " + erg);}
+        else {
+            System.out.println("Ergebnis ist 0");
+        }
+
+    }
+
 }
+
